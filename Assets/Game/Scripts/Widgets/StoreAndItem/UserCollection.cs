3c4cb01ae484d3f24276f4c70fee3494a9417856
edithakarina <editha.karina@gmail.com>
2022-09-16 19:48:30 +0800

    user item collection first commit

---
+++ Assets/Game/Scripts/Widgets/StoreAndItem/UserCollection.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserCollection : MonoBehaviour
+{
+    [SerializeField] GameObject collectionPanel;
+    [SerializeField] GameObject collectionContent;
+    [SerializeField] GameObject itemPrefab;
+    [SerializeField] User user;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Game/Scripts/Widgets/StoreAndItem/UserItem.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UserItem : MonoBehaviour
+{
+    [SerializeField] GameObject firstItem;
+    [SerializeField] GameObject secondItem;
+    [SerializeField] Button chooseFirst;
+    [SerializeField] Button chooseSecond;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
