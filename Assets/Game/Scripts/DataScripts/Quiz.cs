c1e11790061f0451ca3d23915ec0e6667e20ca41
Kabir Kashif <kkas0007@student.monash.edu>
2022-09-20 17:51:29 +0800

    added some functionality to quiz page

---
+++ Assets/Game/Scripts/DataScripts/Quiz.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "Quiz")]
+public class Quiz : ScriptableObject
+{
+    public string quiz_id = "placeholder_id";
+}
