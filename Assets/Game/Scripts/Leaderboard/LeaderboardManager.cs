ef3574312f401f877434c916dbc45f7eb35b83e9
Kabir Kashif <kkas0007@student.monash.edu>
2022-10-21 11:23:23 +0800

    added some code comments

--- Assets/Game/Scripts/Leaderboard/LeaderboardManager.cs
+++ Assets/Game/Scripts/Leaderboard/LeaderboardManager.cs
@@ -22,20 +22,26 @@ public class LeaderboardManager : MonoBehaviour
     public void LoadLeaderboardData()
     {
         int i = 1;
+        // User data from collection, order by points descending
         Query allUsersQuery = db.Collection("User").OrderByDescending("Points");
         allUsersQuery.GetSnapshotAsync().ContinueWithOnMainThread(task =>
         {
+        // loop through data
         QuerySnapshot allUsersQuerySnapshot = task.Result;
         foreach (DocumentSnapshot documentSnapshot in allUsersQuerySnapshot.Documents)
         {
+            // If data exists, only top 10 users are shown
             if (documentSnapshot.Exists && i <=10)
             {
+                // Creates a rung to display user, position, and points
                 GameObject newLBRung = Instantiate(LBRung, LBRungParent);
                 Dictionary<string, object> user = documentSnapshot.ToDictionary();
                 string username = user["Username"].ToString();
                 string points = user["Points"].ToString();
                 newLBRung.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = username;
                 newLBRung.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = points;
+
+                // for top 3 users, gold, silver and bronze medals are displayed inplace of thier positions respectively
                 if (i == 1) 
                 { 
                     newLBRung.transform.Find("GoldBadge").gameObject.SetActive(true);
@@ -49,6 +55,7 @@ public class LeaderboardManager : MonoBehaviour
                     newLBRung.transform.Find("BronzeBadge").gameObject.SetActive(true);
                 }
                 else
+                // for the rest, display their position as a number
                 {
                     newLBRung.transform.GetChild(2).GetComponent<TextMeshProUGUI>().text = i.ToString();
                     newLBRung.transform.GetChild(2).gameObject.SetActive(true);
